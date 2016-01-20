b483b6a2a4e84fae9fe144684a15944a5ad81d5f
Jarrod Alexander <jarroda@2020research.com>
2016-01-20 10:55:35 -0600

    Updating build and readme

--- build/aspnet5-version.js
+++ build/aspnet5-version.js
@@ -5,9 +5,8 @@ var exec = require('child_process').exec;
 
 var baseDir = process.env.APPVEYOR_BUILD_FOLDER;
 var buildVersion = process.env.APPVEYOR_BUILD_VERSION;
-var isRelease = process.env.APPVEYOR_REPO_BRANCH  == 'master';
 var buildNumber = process.env.APPVEYOR_BUILD_NUMBER;
-var semversion = isRelease ? semver.valid(buildVersion) : semver.valid(buildVersion + '-pre-' + buildNumber)
+var semversion = semver.valid(buildVersion + '-pre-' + buildNumber)
 
 // Update current build version
 var cmd = "powershell -command \"& {&'Update-AppveyorBuild' -Version '" + semversion + "'}\"";
@@ -32,17 +31,3 @@ glob(baseDir + "/**/project.json", null, function(er, files) {
         });
     });
 });
-
-if (isRelease) {
-    var nextVersion = semver.inc(buildVersion, 'patch');
-
-    // Modifiy appveyor.yml
-    var pscmd = "powershell -command \"& {(Get-Content appveyor.yml) -replace 'version: \\d+.\\d+.\\d+', 'version: " + nextVersion + "' | Out-File appveyor.yml}";
-    console.info("Incrementing app version.");
-    console.info("Modify command: " + pscmd);
-    exec(pscmd, function(error, stdout, stderr) {
-        if(error) {
-            console.error("Error modifying appveyor.yml: " + error);
-        }
-    });
-}
