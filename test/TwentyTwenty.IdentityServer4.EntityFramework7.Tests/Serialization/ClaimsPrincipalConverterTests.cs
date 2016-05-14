62ef445b523fcc761d74176df1099d61092b417c
Mick dfgvdf <Mick dfgvdf>
2016-05-14 23:00:49 +1200

    CI build fix

--- test/TwentyTwenty.IdentityServer4.EntityFramework7.Tests/Serialization/ClaimsPrincipalConverterTests.cs
+++ test/TwentyTwenty.IdentityServer4.EntityFramework7.Tests/Serialization/ClaimsPrincipalConverterTests.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System.Linq;
 using System.Security.Claims;
+using IdentityModel;
 using TwentyTwenty.IdentityServer4.EntityFramework7.Serialization;
 using Xunit;
 
@@ -13,11 +14,11 @@ namespace TwentyTwenty.IdentityServer4.EntityFramework7.Tests.Serialization
         public void CanSerializeAndDeserializeAClaimsPrincipal()
         {
             var claims = new Claim[]{
-                new Claim(Constants.ClaimTypes.Subject, "alice"),
-                new Claim(Constants.ClaimTypes.Scope, "read"),
-                new Claim(Constants.ClaimTypes.Scope, "write"),
+                new Claim(JwtClaimTypes.Subject, "alice"),
+                new Claim(JwtClaimTypes.Scope, "read"),
+                new Claim(JwtClaimTypes.Scope, "write"),
             };
-            var ci = new ClaimsIdentity(claims, Constants.AuthenticationMethods.Password);
+            var ci = new ClaimsIdentity(claims, OidcConstants.AuthenticationMethods.Password);
             var cp = new ClaimsPrincipal(ci);
 
             var settings = new JsonSerializerSettings();
@@ -25,11 +26,11 @@ namespace TwentyTwenty.IdentityServer4.EntityFramework7.Tests.Serialization
             var json = JsonConvert.SerializeObject(cp, settings);
 
             cp = JsonConvert.DeserializeObject<ClaimsPrincipal>(json, settings);
-            Assert.Equal(Constants.AuthenticationMethods.Password, cp.Identity.AuthenticationType);
+            Assert.Equal(OidcConstants.AuthenticationMethods.Password, cp.Identity.AuthenticationType);
             Assert.Equal(3, cp.Claims.Count());
-            Assert.True(cp.HasClaim(Constants.ClaimTypes.Subject, "alice"));
-            Assert.True(cp.HasClaim(Constants.ClaimTypes.Scope, "read"));
-            Assert.True(cp.HasClaim(Constants.ClaimTypes.Scope, "write"));
+            Assert.True(cp.HasClaim(JwtClaimTypes.Subject, "alice"));
+            Assert.True(cp.HasClaim(JwtClaimTypes.Scope, "read"));
+            Assert.True(cp.HasClaim(JwtClaimTypes.Scope, "write"));
         }
     }
 }
