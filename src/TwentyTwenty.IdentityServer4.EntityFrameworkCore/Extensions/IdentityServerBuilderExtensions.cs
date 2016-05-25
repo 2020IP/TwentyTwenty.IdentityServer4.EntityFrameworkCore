24f7e81248c8f237b5b7f12b5d17f3dec51cf7d4
Eric Green <edgeric@gmail.com>
2016-05-25 08:25:44 -0500

    Removed type parameter from Configure extension

--- src/TwentyTwenty.IdentityServer4.EntityFrameworkCore/Extensions/IdentityServerBuilderExtensions.cs
+++ src/TwentyTwenty.IdentityServer4.EntityFrameworkCore/Extensions/IdentityServerBuilderExtensions.cs
@@ -5,8 +5,7 @@ namespace TwentyTwenty.IdentityServer4.EntityFrameworkCore.Extensions
 {
     public static class IdentityServerBuilderExtensions
     {
-        public static EntityFrameworkOptions ConfigureEntityFramework<TKey>(this IIdentityServerBuilder builder)
-            where TKey : IEquatable<TKey>
+        public static EntityFrameworkOptions ConfigureEntityFramework(this IIdentityServerBuilder builder)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
             return new EntityFrameworkOptions(builder);
